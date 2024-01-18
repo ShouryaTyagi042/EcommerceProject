7fb311d582626f3632d645a506e4a839077a2403
Alankrit15 <alankritmishra283@gmail.com>
2024-01-18 13:58:59 +0530

    Changed the seller model

--- Server/models/seller.js
+++ Server/models/seller.js
@@ -14,32 +14,15 @@ const sellerSchema = new mongoose.Schema({
       type: String,
       required: true
     },
-    products: [
-      {
-        name: {
-          type: String,
-          required: true
-        },
-        description:{
-            type:String,
-            required:true
-        },
-        price: {
-          type: Number,
-          required: true
-        },
-        // Other product-related information can be added here
-      }
-    ],
-    address: {
-        type:String,
-        required:true
-    },
     company_name:{
         type:String,
         required:true
     },
-    contact:{
+    address: {
+        type:String,
+        required:true
+    },
+    mobile:{
         type:String,
         required:true
     }
