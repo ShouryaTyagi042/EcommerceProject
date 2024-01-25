cbefc65f955a683e2dbe4c1241951e7e6ac3844e
D-4 <piyush.singh1315@gmail.com>
2024-01-26 00:24:03 +0530

    updated cart model

--- Server/models/cart.js
+++ Server/models/cart.js
@@ -9,14 +9,6 @@ const cartItemSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
-  paymentSettled: {
-    type: Boolean,
-    default: false,
-  },
-  checkoutTime: {
-    type: Date,
-    default: Date.now,
-  },
 });
 
 const cartSchema = new mongoose.Schema({
