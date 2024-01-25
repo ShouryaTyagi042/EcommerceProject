42a5964bb2002d8074eaa8db82650f79ffd0e74a
Alankrit15 <alankritmishra283@gmail.com>
2024-01-25 20:55:00 +0530

    created seller login route

--- Server/controller/sellerController.js
+++ Server/controller/sellerController.js
@@ -1,14 +1,32 @@
 import Seller from "../models/seller.js";
-
+import { hashPassword } from "../utility/hashpass.js";
+import { checkSeller } from "../services/seller.js";
+import { genAuthToken } from "../utility/genToken.js";
+import { findSeller } from "../services/seller.js";
 export const createSeller = async (req, res) => {
     try {
       console.log(req.body);
       const { name, email, password,company_name,address, mobile } = req.body;
-      const seller = await Seller.create({ name, email, password, company_name,address, mobile });
+      if (await checkSeller(email)) throw new Error("user already exists");
+      const pass = await hashPassword(password);
+      const seller = await Seller.create({ name, email, password:pass, company_name,address, mobile });
       console.log(seller);
       res.status(201).send({ seller });
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
   };
+  export const loginSeller = async (req, res) => {
+    try {
+        const {email, password } = req.body;
+        const seller = await findSeller(email, password);
+        console.log(seller)
+        const token = genAuthToken(seller._id);
+        console.log(seller)
+        res.status(200).send({ seller, token })
+    } catch (error) {
+        res.status(401).json({ error: error.message });
+    }
+  }
+  
   
