d4087a30fa06952a36756f33cedeedd514f25173
Alankrit15 <alankritmishra283@gmail.com>
2024-01-31 23:11:20 +0530

    modified user schema

--- Server/models/user.js
+++ Server/models/user.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  balance:{
+    type:Number,
+    default:10000
+  },
   address: {
     type: String,
     required: true,
