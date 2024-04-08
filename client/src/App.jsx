7fbc44293775cd7addef6778848fd88ba1e5b625
Alankrit15 <alankritmishra283@gmail.com>
2024-04-08 15:24:38 +0530

    initiated the frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+//components
+import Header from './components/header/Header'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <Header/>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
