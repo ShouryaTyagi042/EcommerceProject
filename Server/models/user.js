450c6fa86b550b8ce3871a4b3986d0cd2caa389a
Alankrit15 <alankritmishra283@gmail.com>
2024-04-09 23:29:47 +0530

    modified user model

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,28 +1,42 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: {
+  firstname: {
     type: String,
     required: true,
+    trim:true,
+  },
+  lastname:{
+    type:String,
+    required:true,
+    trim:true,
+  },
+  username:{
+    type:String,
+    required:true,
+    trim:true,
+    unique:true,
+    index:true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    trim:true,
   },
   password: {
     type: String,
     required: true,
   },
-  balance:{
-    type:Number,
-    default:10000
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  mobile: {
+  // balance:{
+  //   type:Number,
+  //   default:10000
+  // },
+  // address: {
+  //   type: String,
+  //   required: true,
+  // },
+  phone: {
     type: String,
     required: true,
   },
