af2ff203807fc10245daa7cae0dde776765b080c
Alankrit15 <alankritmishra283@gmail.com>
2024-04-09 16:10:40 +0530

    created navbar component

---
+++ client/src/components/Home/NavBar.jsx
@@ -0,0 +1,39 @@
+import { Box,Typography,styled } from "@mui/material";
+
+
+import { navData } from "../../constants/data";
+
+const ModifiedNavBar= styled(Box)`
+display:flex;
+margin:55px 130px 0px 130px;
+justify-content:space-between;
+`
+const Conatiner=styled(Box)`
+padding:12px 8px;
+text-align:center;
+`
+
+const Text=styled(Typography)`
+font-size:12px;
+font-weight:575;
+font-family: inter_semi_bold, fallback-inter_semi_bold, Arial, sans-serif;
+`
+
+
+
+const NavBar=()=>{
+    return (
+        <ModifiedNavBar>
+            {
+                navData.map(data=> (
+                    <Conatiner  >
+                        <img src={data.url} alt="nav" style={{width:64}}/>
+                        <Text>{data.text}</Text>
+                    </Conatiner>
+                ))
+            }
+        </ModifiedNavBar>
+        )
+}
+
+export default NavBar;
