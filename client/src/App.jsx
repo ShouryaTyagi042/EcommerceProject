82a4c291e2d4c4f309457a6d29ed5ee2ea728453
D-4 <piyush.singh1315@gmail.com>
2024-04-12 15:21:52 +0530

    added routing, detailed view page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,19 +5,26 @@ import Header from './components/header/Header'
 import Home from './components/Home/Home'
 import DataProvider from './context/DataProvider'
 import { Box } from '@mui/material'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import DetailView from './components/details/DetailView'
 
 function App() {
 
   return (
     <>
       <DataProvider>
-       <Header/>
-       <Box style={{
-        marginTop:54
-       }}>
-       <Home/>
-       </Box>
-       
+      <BrowserRouter>
+        <Header/>
+        <Box style={{
+          marginTop:54
+        }}>
+        <Routes>
+          <Route path="/" element = {<Home/>} />
+          <Route path="/product/:id" element = {<DetailView/>} />
+          {/* <DetailView /> */}
+        </Routes>
+        </Box>
+       </BrowserRouter>
       </DataProvider>
     </>
   )
