5a3f6ea61f51ae681c2d55be2317ce577a9dd1cf
D-4 <piyush.singh1315@gmail.com>
2024-04-13 23:42:55 +0530

    Added new GET route for fetching product via ID

--- Server/routes/productRoute.js
+++ Server/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express, { Router } from "express";
 import { createNewProduct } from "../controller/productController.js";
-import { getProducts } from "../controller/productController.js";
+import { getProducts, getProductsById } from "../controller/productController.js";
 const router = express.Router();
 
 
 router.get("/products",getProducts);
 router.post("/add-new-product", createNewProduct);
+router.get("/product/:id", getProductsById);
 
 
 export default router
