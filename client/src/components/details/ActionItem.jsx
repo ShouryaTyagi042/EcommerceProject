d2ee16f24fb09481fe0da233c338506060357f74
D-4 <piyush.singh1315@gmail.com>
2024-04-14 18:50:10 +0530

    Added logic to the addtocart button

--- client/src/components/details/ActionItem.jsx
+++ client/src/components/details/ActionItem.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import { useState } from 'react'
 import { Box, Button, styled } from '@mui/material'
+
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/actions/cartActions';
 
 const LeftContainer = styled(Box)(({ theme }) => ({
   minWidth: '40%',
@@ -22,11 +26,28 @@ const LeftContainer = styled(Box)(({ theme }) => ({
 
 
  const ActionItem = ({ product }) => {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [quantity, setQuantity] = useState(1)
+
+  const { id } = product;
+
+  const addItemsToCart = () => {
+      navigate('/cart');
+  }
+
   return (
     <LeftContainer>
     <Box style={{padding: '30px 20px 15px', border: '1px solid #f0f0f0'}}>
         <Image src={product.url} alt={product.img} />
-        <StyledButton variant='contained' style={{marginRight: 10}}>Add to Cart</StyledButton>
+        <StyledButton variant='contained' style={{marginRight: 10}} 
+        onClick={() =>{
+          dispatch(addToCart(id, quantity))
+          navigate('/cart');
+        }}>
+          Add to Cart</StyledButton>
         <StyledButton variant='contained' style={{background: '#fb541b'}}>Buy Now</StyledButton>
     </Box>
     </LeftContainer>
