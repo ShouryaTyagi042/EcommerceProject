dcd70b22bcd59bab7f95b463f61cd47bfa5255d6
D-4 <piyush.singh1315@gmail.com>
2024-04-14 18:48:39 +0530

    added cart route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import DataProvider from './context/DataProvider'
 import { Box } from '@mui/material'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import DetailView from './components/details/DetailView'
+import Cart from './components/cart/Cart'
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element = {<Home/>} />
           <Route path="/product/:id" element = {<DetailView/>} />
+          <Route path ="/cart" element= {<Cart/>} />
           {/* <DetailView /> */}
         </Routes>
         </Box>
