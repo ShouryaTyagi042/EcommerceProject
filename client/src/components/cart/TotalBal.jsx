1dc95e529c5ae7b46e11fd6c2ca88eb89a5e4890
D-4 <piyush.singh1315@gmail.com>
2024-04-14 19:40:08 +0530

    This component wil display the final amount to the consumer

--- client/src/components/cart/TotalBal.jsx
+++ client/src/components/cart/TotalBal.jsx
@@ -1,9 +1,31 @@
+import { Typography, Box } from '@mui/material'
 import React from 'react'
 
-export default function TotalBal() {
+export default function TotalBal( { cartItems }) {
   return (
-    <div>
-      This is where all the money will be shown 
-    </div>
+    <Box>
+      <Box>
+        <Box>
+        <Typography>
+            Price Details
+        </Typography>
+        </Box>
+        <Box>
+            <Typography>Price ({cartItems?.length} item)
+                <Box component = "span">100</Box>
+                </Typography>
+
+                <Typography>Delivery Charges ({cartItems?.length} item)
+                <Box component = "span">100</Box>
+                </Typography>
+
+                <Typography>Total Amount ({cartItems?.length} item)
+                <Box component = "span">100</Box>
+                </Typography>
+
+                {/* <Typography></Typography> */}
+        </Box>
+        </Box> 
+    </Box>
   )
 }
