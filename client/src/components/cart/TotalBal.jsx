d6165ee0549d32f9f25db4454ecbd9a19ad2716e
D-4 <piyush.singh1315@gmail.com>
2024-04-14 23:15:06 +0530

    Page for calculating and displaying the total amount during checkout

--- client/src/components/cart/TotalBal.jsx
+++ client/src/components/cart/TotalBal.jsx
@@ -1,31 +1,51 @@
-import { Typography, Box } from '@mui/material'
-import React from 'react'
+import { Typography, Box, Paper } from '@mui/material'
+import { useState, useEffect } from 'react'
 
 export default function TotalBal( { cartItems }) {
+
+    const [price, setPrice] = useState(0);
+    const [delCharge, setDelCharge] = useState(40);
+
+    useEffect(() => {
+        totalAmt();
+    }, [cartItems])
+
+    // let delCharge = 40;
+
+    const totalAmt = () => {
+        let total = 0;
+        cartItems.map(item => {
+            total = total + item.price.cost;
+        })
+        setPrice(total);
+        
+        //Free Delivery for items above 500
+        if(price > 500) setDelCharge(0);
+    }
   return (
-    <Box>
-      <Box>
-        <Box>
-        <Typography>
-            Price Details
+    <Box component={Paper} p={2} elevation={2}>
+      <Box mb={2}>
+        <Typography variant="h6">
+          Price Details
         </Typography>
-        </Box>
-        <Box>
-            <Typography>Price ({cartItems?.length} item)
-                <Box component = "span">100</Box>
-                </Typography>
-
-                <Typography>Delivery Charges ({cartItems?.length} item)
-                <Box component = "span">100</Box>
-                </Typography>
-
-                <Typography>Total Amount ({cartItems?.length} item)
-                <Box component = "span">100</Box>
-                </Typography>
-
-                {/* <Typography></Typography> */}
-        </Box>
-        </Box> 
+      </Box>
+      <Box mb={2}>
+  <Typography variant="subtitle1" mb={2} sx={{ fontFamily: 'sans-serif' }}>
+    Price ({cartItems?.length} items)
+    <Box component="span" ml={2}>₹{price}</Box>
+  </Typography>
+  <Typography variant="subtitle1" mb={2} sx={{ fontFamily: 'sans-serif' }}>
+    Delivery Charges ({cartItems?.length} items)
+    <Box component="span" ml={2}>₹{delCharge}</Box>
+  </Typography>
+  <Typography variant="h5" color="primary" mb={2} sx={{ fontFamily: 'sans-serif' }}>
+    Total Amount ({cartItems?.length} items)
+    <Box component="span" ml={2}>₹{price + delCharge}</Box>
+  </Typography>
+  <Typography sx={{ color: 'green', fontWeight: 'lighter', mt: 2, fontFamily: 'sans-serif' }}>
+    Free Delivery on orders above ₹ 500
+  </Typography>
+</Box>
     </Box>
   )
 }
