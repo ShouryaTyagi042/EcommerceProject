302fd902a3aed0bdd61d9350298af9b8141794f4
Alankrit15 <alankritmishra283@gmail.com>
2024-04-15 22:40:20 +0530

    modified seller services in the Server

--- Server/services/seller.js
+++ Server/services/seller.js
@@ -1,8 +1,8 @@
 import Seller from "../models/seller.js"
 import { checkPass } from "../utility/hashpass.js"
 // import { hashPassword } from "../utility/hashpass.js"
-export const findSeller = async (email, password) => {
-    const seller = await Seller.findOne({ email })
+export const findSeller = async (username, password) => {
+    const seller = await Seller.findOne({ username })
     if (!seller) {
         throw new Error('Seller does not exist')
     }
@@ -15,8 +15,8 @@ export const findSeller = async (email, password) => {
     return seller
 }
 
-export const checkSeller = async (email) => {
-    const check = await Seller.findOne({ email });
+export const checkSeller = async (username) => {
+    const check = await Seller.findOne({ username });
     if (check) return true;
     return false
 }
