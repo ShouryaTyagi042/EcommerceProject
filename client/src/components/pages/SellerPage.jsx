302b9f2956e0e7a36023badb88f47843310fb935
Piyush Singh <95350374+Darth-InVader15@users.noreply.github.com>
2024-04-26 02:02:27 +0530

    Created the seller overview page

--- client/src/components/pages/SellerPage.jsx
+++ client/src/components/pages/SellerPage.jsx
@@ -1,10 +1,31 @@
+import React from 'react';
+import { FaRegListAlt, FaPlusSquare } from 'react-icons/fa';
+import './styles/SellerPage.css';
 
+const SellerPage = () => {
+  return (
+    <div>
+      <div className="title">
+        <h1>Welcome to the Seller Dashboard</h1>
+      </div>
 
-
-const SellerPage=()=>{
-    return(
-        <p>Hi from seller page</p>
-    )
-}
+      <div> 
+        <h3>Total Income = Rs. 0</h3>
+      </div>
+      {/* Hard coded this line, will change later */}
+      
+      <div className="button-container">
+        <button className="button">
+          <FaRegListAlt size={88} style={{ marginBottom: '30px' }} />
+          Show all my products
+        </button>
+        <button className="button">
+          <FaPlusSquare size={88} style={{ marginBottom: '30px' }} />
+          Add new products
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default SellerPage;
