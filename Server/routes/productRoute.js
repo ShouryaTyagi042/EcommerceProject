92f420cc6b023a0af0f805c09f413a4dae155929
Piyush Singh <95350374+Darth-InVader15@users.noreply.github.com>
2024-04-27 11:30:46 +0530

    Add seller products route

--- Server/routes/productRoute.js
+++ Server/routes/productRoute.js
@@ -1,6 +1,8 @@
 import express, { Router } from "express";
 import { allProductsDetails, createNewProduct } from "../controller/productController.js";
 import { getProducts, getProductsById } from "../controller/productController.js";
+
+import Product from "../models/product.js";
 const router = express.Router();
 
 
@@ -8,5 +10,15 @@ router.get("/products",getProducts);
 router.post("/add-new-product", createNewProduct);
 router.get("/product/:id", getProductsById);
 router.get("/all-products",allProductsDetails);
+
+
+router.get('/seller/:sellerId', async (req, res) => {
+    try {
+      const products = await Product.find({ sellerId: req.params.sellerId });
+      res.json(products);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
 export default router
