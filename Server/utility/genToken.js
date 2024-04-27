3f2d4bf42486e25a17f72d5b36f0154bd55f20d3
Piyush Singh <95350374+Darth-InVader15@users.noreply.github.com>
2024-04-27 11:28:08 +0530

    minor bug fixed

--- Server/utility/genToken.js
+++ Server/utility/genToken.js
@@ -1,3 +1,3 @@
 import "dotenv/config"
 import jsonwebtoken from "jsonwebtoken"
-export const genAuthToken = (name, email) => jsonwebtoken.sign({ name, email}, process.env.JWT_SECRET) 
+export const genAuthToken = (name, email) => jsonwebtoken.sign({ name, email}, `${process.env.JWT_SECRET}`) 
