9bee72b811b2af6563966a3dd376a7545e73016e
Piyush Singh <95350374+Darth-InVader15@users.noreply.github.com>
2024-04-27 11:26:14 +0530

    Created the seller page

--- client/src/components/pages/SellerPage.jsx
+++ client/src/components/pages/SellerPage.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { FaRegListAlt, FaPlusSquare } from 'react-icons/fa';
 import './styles/SellerPage.css';
+import { useContext, useEffect } from "react";
+import { DataContext } from '../../context/DataProvider';
+import { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const SellerPage = () => {
+  const navigate = useNavigate();
+
+  const handleButtonAllProducts = () => {
+    navigate('/allproducts');
+  };
+
+
+  const {log, sellerDetail} = useContext(DataContext);
+  console.log("This is just a test " + sellerDetail.firstname);
   return (
     <div>
       <div className="title">
-        <h1>Welcome to the Seller Dashboard</h1>
+        <h1>Welcome {sellerDetail.firstname}</h1>
+        
       </div>
 
       <div> 
@@ -15,7 +30,7 @@ const SellerPage = () => {
       {/* Hard coded this line, will change later */}
       
       <div className="button-container">
-        <button className="button">
+        <button className="button" onClick={handleButtonAllProducts}>
           <FaRegListAlt size={88} style={{ marginBottom: '30px' }} />
           Show all my products
         </button>
