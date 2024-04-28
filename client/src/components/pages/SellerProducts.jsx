84bb91980d865e6375c35af16e6ef4da19f85562
Piyush Singh <95350374+Darth-InVader15@users.noreply.github.com>
2024-04-28 23:56:22 +0530

    This commit updates the display of the seller's products from a list of div elements to a Material-UI Table component. Also, updated searchBySellerID to searchBySeller Mail

--- client/src/components/pages/SellerProducts.jsx
+++ client/src/components/pages/SellerProducts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useContext, useEffect, useState } from 'react';
 import { DataContext } from '../../context/DataProvider';
 import './styles/SellerProducts.css';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import axios from 'axios';
 const URL="http://localhost:5000";
 
@@ -10,36 +11,53 @@ const SellerProducts = () => {
 //   const products = sellerDetail.products;
   const [products, setProducts] = useState([]);
   const id = sellerDetail._id;
-
+  const mail = sellerDetail.email;
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get(URL + `/products/seller/${id}`);
+        const response = await axios.get(URL + `/products/${mail}`);
         setProducts(response.data);
+        console.log(response.data);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchProducts();
-  }, [id]);
+  }, []);
 
   return (
     <div>
       <h1 className="title">Here are all your products</h1>
-      <div className="container">
-        {!products || products.length === 0 ? (
-          <p className='emptylist'>There is nothing to show here, start adding some products</p>
-        ) : (
-          products.map((product, index) => (
-            <div key={index} className="product">
-              <h2>{product.name}</h2>
-              <p>{product.description}</p>
-              <p>{product.price}</p>
-            </div>
-          ))
-        )}
-      </div>
+  <div className="container">
+    {!products || products.length === 0 ? (
+      <p className='emptylist'>There is nothing to show here, start adding some products</p>
+    ) : (
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Listed Price</TableCell>
+              <TableCell align="right">Remaining Quantity</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product, index) => (
+              <TableRow key={index}>
+                <TableCell component="th" scope="row">
+                  {product.title.shortTitle}
+                </TableCell>
+                <TableCell align="right">Rs. {product.price.mrp}</TableCell>
+                <TableCell align="right">{product.remainingQuantity}</TableCell>
+                <TableCell align="left">{product.quantity}</TableCell> 
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    )}
+  </div>
     </div>
   );
 };
